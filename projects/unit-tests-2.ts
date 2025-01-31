76fe2bb0de1404ab7ef202fcb913e391461a7447
KennochaJilian <52425312+KennochaJilian@users.noreply.github.com>
2025-01-31 11:51:30 +0100

    Update unit-tests-2.ts

--- projects/unit-tests-2.ts
+++ projects/unit-tests-2.ts
@@ -1,8 +1,64 @@
-      TypeError: Cannot read properties of undefined (reading 'navigationId')
-            at Object.get navigationId [as navigationId] (node_modules/@angular/router/fesm2020/router.mjs:4979:43)
-            at forEach (projects/webo-mockslib/src/lib/webo/mock-provider.ts:34:91)
-            at Array.forEach (<anonymous>)
-            at MockProvider.autoMock (projects/webo-mockslib/src/lib/webo/mock-provider.ts:32:18)
-            at new MockProvider (projects/webo-mockslib/src/lib/webo/mock-provider.ts:11:28)
-            at WeboSandboxTestBed.buildProviders (projects/webo-mockslib/src/lib/webo/abstract-testbed.ts:54:11)
-            at WeboSandboxTestBed.WEBOSANDBOX_TEST_BED [as forComponent] (projects/webo-mockslib/src/lib/webo/abstract-testbed.ts:37:30)
+<app-table-sandbox _nghost-fio-c164=""><h2 _ngcontent-fio-c164="">Tableaux</h2><p _ngcontent-fio-c164=""> Permet aussi de tester le WeboServiceHttp. Lire le Readme de cette sandbox pour utiliser json server
+</p><webo-table _ngcontent-fio-c164="" _nghost-fio-c160="" ng-reflect-config="[object Object]"><mat-form-field _ngcontent-fio-c160="" class="mat-mdc-form-field search-bar ng-tns-c86-0 mat-mdc-form-field-type-mat-input mat-form-field-appearance-fill mat-form-field-hide-placeholder mat-primary ng-star-inserted"><!--container--><div class="mat-mdc-text-field-wrapper mdc-text-field ng-tns-c86-0 mdc-text-field--filled"><div class="mat-mdc-form-field-focus-overlay ng-tns-c86-0 ng-star-inserted"></div><!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><div class="mat-mdc-form-field-flex ng-tns-c86-0"><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--><div class="mat-mdc-form-field-infix ng-tns-c86-0"><label matformfieldfloatinglabel="" class="mdc-floating-label mat-mdc-floating-label ng-tns-c86-0 ng-star-inserted" ng-reflect-floating="false" ng-reflect-disabled="true" id="mat-mdc-form-field-label-0" for="mat-input-0" aria-owns="mat-input-0"><mat-label _ngcontent-fio-c160="" class="ng-tns-c86-0">Rechercher une prévision</mat-label><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--></label><!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><!--bindings={
+  "ng-reflect-ng-template-outlet": "[object Object]"
+}--><!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><input _ngcontent-fio-c160="" matinput="" class="mat-mdc-input-element ng-tns-c86-0 mat-mdc-form-field-input-control mdc-text-field__input cdk-text-field-autofill-monitored" ng-reflect-placeholder="Ex." placeholder="Ex." id="mat-input-0" aria-invalid="false" aria-required="false"></div><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--></div><div matformfieldlineripple="" class="mdc-line-ripple ng-tns-c86-0 mdc-line-ripple--deactivating ng-star-inserted"></div><!--bindings={
+  "ng-reflect-ng-if": "true"
+}--></div><div class="mat-mdc-form-field-subscript-wrapper mat-mdc-form-field-bottom-align ng-tns-c86-0" ng-reflect-ng-switch="hint"><!--bindings={
+  "ng-reflect-ng-switch-case": "error"
+}--><div class="mat-mdc-form-field-hint-wrapper ng-tns-c86-0 ng-trigger ng-trigger-transitionMessages ng-star-inserted" style="opacity: 1; transform: translateY(0%);"><!--bindings={
+  "ng-reflect-ng-if": ""
+}--><div class="mat-mdc-form-field-hint-spacer ng-tns-c86-0"></div></div><!--bindings={
+  "ng-reflect-ng-switch-case": "hint"
+}--></div></mat-form-field><!--bindings={
+  "ng-reflect-ng-if": "[object Object]"
+}--><h3 _ngcontent-fio-c160="" class="nb-results ng-star-inserted"> Prévisions météos trouvées (6)</h3><!--bindings={
+  "ng-reflect-ng-if": "Prévisions météos trouvées"
+}--><table _ngcontent-fio-c160="" mat-table="" class="mat-mdc-table mdc-data-table__table cdk-table mat-elevation-z8 ng-star-inserted" style="background-color: #e9eef5;" role="table" ng-reflect-data-source="[object Object]"><thead role="rowgroup"><tr _ngcontent-fio-c160="" role="row" mat-header-row="" class="mat-mdc-header-row mdc-data-table__header-row cdk-header-row header-text ng-star-inserted" style="background-color: #4170a9;"><th _ngcontent-fio-c160="" role="columnheader" mat-header-cell="" class="mat-mdc-header-cell mdc-data-table__header-cell cdk-header-cell cdk-column-date mat-column-date cell-left header-cell w-3 ng-star-inserted mat-mdc-table-sticky mat-mdc-table-sticky-border-elem-top" style="top: 0px; z-index: 100;"> Date <!--bindings={}--><webo-sort-button _ngcontent-fio-c160="" _nghost-fio-c156="" ng-reflect-current-column="[object Object]" ng-reflect-is-active="false" ng-reflect-is-hover="false" class="ng-star-inserted"><span _ngcontent-fio-c156="" class="sort-btn" ng-reflect-ng-class="[object Object]"><mat-icon _ngcontent-fio-c156="" role="img" class="mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color ng-star-inserted" aria-hidden="true" data-mat-icon-type="font">arrow_downward</mat-icon><!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><!--bindings={
+  "ng-reflect-ng-if-else": "[object Object]"
+}--><!--container--></span></webo-sort-button><!--ng-container--><!--bindings={
+  "ng-reflect-ng-if": "[object Object]"
+}--></th><th _ngcontent-fio-c160="" role="columnheader" mat-header-cell="" class="mat-mdc-header-cell mdc-data-table__header-cell cdk-header-cell cdk-column-temperatureC mat-column-temperatureC cell-left header-cell ng-star-inserted mat-mdc-table-sticky mat-mdc-table-sticky-border-elem-top" style="top: 0px; z-index: 100;"> Temperature en °C <!--bindings={}--><!--bindings={}--></th><th _ngcontent-fio-c160="" role="columnheader" mat-header-cell="" class="mat-mdc-header-cell mdc-data-table__header-cell cdk-header-cell cdk-column-temperatureF mat-column-temperatureF cell-left header-cell ng-star-inserted mat-mdc-table-sticky mat-mdc-table-sticky-border-elem-top" style="top: 0px; z-index: 100;"> Temperature en °F <!--bindings={}--><!--bindings={}--></th><th _ngcontent-fio-c160="" role="columnheader" mat-header-cell="" class="mat-mdc-header-cell mdc-data-table__header-cell cdk-header-cell cdk-column-summary mat-column-summary cell-left header-cell ng-star-inserted mat-mdc-table-sticky mat-mdc-table-sticky-border-elem-top" style="top: 0px; z-index: 100;"> Résumé <!--bindings={}--><!--bindings={}--></th><!--ng-container--></tr><!--ng-container--></thead><tbody role="rowgroup" class="mdc-data-table__content"><tr _ngcontent-fio-c160="" role="row" mat-row="" class="mat-mdc-row mdc-data-table__row cdk-row header-text ng-star-inserted"><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-date mat-column-date cell-left ng-star-inserted" role="cell"> 2025-01-01 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureC mat-column-temperatureC cell-left ng-star-inserted" role="cell"> 0 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureF mat-column-temperatureF cell-left ng-star-inserted" role="cell"> 10 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-summary mat-column-summary cell-left ng-star-inserted" role="cell"> Il fait très froid </td><!--ng-container--></tr><tr _ngcontent-fio-c160="" role="row" mat-row="" class="mat-mdc-row mdc-data-table__row cdk-row header-text ng-star-inserted"><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-date mat-column-date cell-left ng-star-inserted" role="cell"> 2025-01-22 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureC mat-column-temperatureC cell-left ng-star-inserted" role="cell"> 5 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureF mat-column-temperatureF cell-left ng-star-inserted" role="cell"> 24 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-summary mat-column-summary cell-left ng-star-inserted" role="cell"> Brumeux </td><!--ng-container--></tr><tr _ngcontent-fio-c160="" role="row" mat-row="" class="mat-mdc-row mdc-data-table__row cdk-row header-text ng-star-inserted"><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-date mat-column-date cell-left ng-star-inserted" role="cell"> 2025-01-21 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureC mat-column-temperatureC cell-left ng-star-inserted" role="cell"> 10 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureF mat-column-temperatureF cell-left ng-star-inserted" role="cell"> 25 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-summary mat-column-summary cell-left ng-star-inserted" role="cell"> Plage et cocotier </td><!--ng-container--></tr><tr _ngcontent-fio-c160="" role="row" mat-row="" class="mat-mdc-row mdc-data-table__row cdk-row header-text ng-star-inserted"><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-date mat-column-date cell-left ng-star-inserted" role="cell"> 2025-01-17 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureC mat-column-temperatureC cell-left ng-star-inserted" role="cell"> 15 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureF mat-column-temperatureF cell-left ng-star-inserted" role="cell"> 28 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-summary mat-column-summary cell-left ng-star-inserted" role="cell"> Coquillage et crustacé </td><!--ng-container--></tr><tr _ngcontent-fio-c160="" role="row" mat-row="" class="mat-mdc-row mdc-data-table__row cdk-row header-text ng-star-inserted"><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-date mat-column-date cell-left ng-star-inserted" role="cell"> 2025-01-16 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureC mat-column-temperatureC cell-left ng-star-inserted" role="cell"> 3 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureF mat-column-temperatureF cell-left ng-star-inserted" role="cell"> 30 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-summary mat-column-summary cell-left ng-star-inserted" role="cell"> Grand soleil </td><!--ng-container--></tr><tr _ngcontent-fio-c160="" role="row" mat-row="" class="mat-mdc-row mdc-data-table__row cdk-row header-text ng-star-inserted"><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-date mat-column-date cell-left ng-star-inserted" role="cell"> 2025-01-13 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureC mat-column-temperatureC cell-left ng-star-inserted" role="cell"> 4 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-temperatureF mat-column-temperatureF cell-left ng-star-inserted" role="cell"> 14 </td><td _ngcontent-fio-c160="" mat-cell="" class="mat-mdc-cell mdc-data-table__cell cdk-cell cdk-column-summary mat-column-summary cell-left ng-star-inserted" role="cell"> Neige </td><!--ng-container--></tr><!--ng-container--><!--ng-container--></tbody><tfoot role="rowgroup" class="mat-mdc-table-sticky" style="display: none; bottom: 0px; z-index: 10;"><!--ng-container--></tfoot></table><!--ng-container--><!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--><!--bindings={
+  "ng-reflect-ng-if": "false"
+}--><!--container--><!--container--></webo-table><!--bindings={
+  "ng-reflect-ng-if": "[object Object]"
+}--></app-table-sandbox>
+
+
+
+
+
+it("display rows when getAll return value", () =>
+    {
+
+        (service.useValue.getAll as jasmine.Spy).and.returnValue(of([{"date": "2025-01-01", "temperatureC": 0, "temperatureF": 10, "summary": "Il fait très froid"},
+            {"date": "2025-01-22", "temperatureC": 5, "temperatureF": 24, "summary": "Brumeux"}]))
+
+        let nbElements = fixture.debugElement.query(By.css(".nb-results"));
+
+        console.log("nbElement",nbElements);
+        // expect(nbElements).toBeTruthy();
+    })
